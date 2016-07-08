5c636fc7ef104bdb7b5a6025d344381f09537a3b
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-07-08 15:23:00 -0400

    grabbing post request code

--- public/main.js
+++ public/main.js
@@ -1,25 +1,26 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = function(app) {
-    app.controller('HomeController',['MusicFactory','$scope','$location',function(MusicFactory,$scope, $location) {
-        $scope.loginClick = function() {
-            console.log('clicked')
-            if ($scope.username != null){
-              $location.path('/home');
-            } else {
-              alert ('Please enter a username');
-            }
-        }
-        $scope.PostThisThing = function(){
+        app.controller('HomeController', ['MusicFactory', '$scope', '$location', function(MusicFactory, $scope, $location) {
+                $scope.loginClick = function() {
+                    console.log('clicked')
+                    if ($scope.username != null) {
+                        $location.path('/home');
+                    } else {
+                        alert('Please enter a username');
+                    }
+                }
 
-        }
-        $scope.bandManagerSelect = function() {
-            console.log('clicked band manger options')
-        }
-        $scope.musicianSelect = function() {
-            console.log('clicked musical instruments')
-        }
-    }]);
-};
+                    $scope.bandManagerSelect = function() {
+                        console.log('clicked band manger options')
+                            // $location.path('/available');
+                    }
+                    $scope.musicianSelect = function($scope, $location) {
+                        console.log('clicked musical instruments')
+                            ////need to make sure they can select multiple before they are redirected to the lookingfor page
+                            // $location.path('/lookingfor');
+                    }
+                }]);
+        };
 
 },{}],2:[function(require,module,exports){
 module.exports = function(app) {
