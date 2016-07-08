45c8910d27cdc91785bc3719d2934f925fd0131c
Robert Suber <robertsuber@Roberts-MacBook-Pro.local>
2016-07-08 15:13:43 -0400

    ch-ch-changes

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = function(app) {
-    app.controller('HomeController', function($scope, $location) {
+    app.controller('HomeController',['MusicFactory','$scope','$location',function(MusicFactory,$scope, $location) {
         $scope.loginClick = function() {
             console.log('clicked')
             if ($scope.username != null){
@@ -9,14 +9,18 @@ module.exports = function(app) {
               alert ('Please enter a username');
             }
         }
+        $scope.PostThisThing = function(){
+
+        }
         $scope.bandManagerSelect = function() {
             console.log('clicked band manger options')
         }
         $scope.musicianSelect = function() {
             console.log('clicked musical instruments')
         }
-    });
+    }]);
 };
+
 },{}],2:[function(require,module,exports){
 module.exports = function(app) {
     app.controller('LookingForController', ['$scope', '$http', function($scope, $http) {
@@ -27,7 +31,6 @@ module.exports = function(app) {
 };
 
 },{}],3:[function(require,module,exports){
-
 let app = angular.module('BandInTreble', ['ngRoute']);
 require('./controllers/homecontroller')(app);
 require('./controllers/lookingforcontroller')(app);
@@ -53,6 +56,26 @@ app.config(['$routeProvider', function($routeProvider) {
             controller: 'LookingForController',
             templateUrl: 'templates/lookingfor.html',
         });
+}]);
+
+//
+// THIS IS THE SERVICE BREH BRO
+//
+app.factory('MusicFactory', ['$http', '$location', function($http) {
+    return {
+      // todo: rename this to be more specific
+        postThis: function() {
+            $http({
+                url: '/musician',
+                method: 'post',
+                data: {
+                    name: 'Logan'
+                },
+            }).then(function(results) {
+                console.log("posted")
+            });
+        },
+    }; // end return
 }]);
 
 },{"./controllers/homecontroller":1,"./controllers/lookingforcontroller":2}]},{},[3])
