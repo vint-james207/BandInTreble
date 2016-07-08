317bb1e2d5d856b1e0f12f03dd582e46948fb9d4
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-07-08 13:37:14 -0400

    click event for login route

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,7 +1,12 @@
 module.exports = function(app) {
-    app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
+    app.controller('HomeController', function($scope, $location) {
         $scope.loginClick = function() {
             console.log('clicked')
+            if ($scope.username != null){
+              $location.path('/home');
+            } else {
+              alert ('Please enter a username');
+            }
         }
-    }]);
+    });
 };
