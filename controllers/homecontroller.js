af7fcb64c92dad581f2fd511a5330e97018cf02a
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-07-09 11:05:25 -0400

    400 error with GET request, sending to back end

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -11,14 +11,14 @@ module.exports = function(app) {
         }
     $scope.bandManagerSelect = function() {
             console.log('clicked band manger options')
-            $location.path('/available');
+            // $location.path('/available');
             // MusicFactory.getMusician.user();
             $scope.musician = MusicFactory.getMusician();
         }
         $scope.musicianSelect = function() {
             console.log('clicked musical instruments')
             ////need to make sure they can select multiple before they are redirected to the lookingfor page
-            $location.path('/lookingfor');
+            // $location.path('/lookingfor');
             // MusicFactory.getBandManager.user();
             $scope.bandmanager = MusicFactory.getBandManager();
 
